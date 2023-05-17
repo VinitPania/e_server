48e10d46f1dd25056049f8d7bf3e548b57f0832a
VinitPania <vinit.pania@cloverinfotech.com>
2023-05-17 17:00:05 +0530

    Changed pom and main so that it can generate war

--- src/main/java/com/eserver/EServerApplication.java
+++ src/main/java/com/eserver/EServerApplication.java
@@ -2,14 +2,21 @@ package com.eserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EServerApplication {
+public class EServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EServerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EServerApplication.class);
 	}
 
 }
